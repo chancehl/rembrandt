16404f8d9e39bbe2a9a69b58eda229f502cd0af7
Chance Linz <clinz.dev@gmail.com>
2023-03-02 18:20:36 -0800

    adding placekitten image embed

--- src/commands/art.command.ts
+++ src/commands/art.command.ts
@@ -1,9 +1,21 @@
-import { CommandInteraction, Client, ApplicationCommandType } from 'discord.js'
+import { CommandInteraction, Client, ApplicationCommandType, Embed, EmbedBuilder } from 'discord.js'
 
 import { Command } from './base.command'
 
+const PHRASES = ['watchu think?', 'u like?', 'lol', 'UwU', 'OwO', 'oh yeah', 'omg', 'chefs kiss']
+
+function pickRandomPhrase() {
+    return PHRASES[Math.floor(Math.random() * PHRASES.length)]
+}
+
 async function execute(_client: Client, interaction: CommandInteraction) {
-    await interaction.followUp({ ephemeral: true, content: 'pong' })
+    // prettier-ignore
+    const embed = new EmbedBuilder()
+        .setImage('http://placekitten.com/200/300')
+        .setTitle('Some kitten')
+        .setDescription('Some kitty')
+
+    await interaction.followUp({ ephemeral: true, content: pickRandomPhrase(), embeds: [embed] })
 }
 
 export const ArtCommand: Command = {
