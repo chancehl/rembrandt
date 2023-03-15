b6ef64c52361af68937d458af87f0cbd0133cff2
Chance Linz <clinz.dev@gmail.com>
2023-03-14 23:59:27 -0700

    🐛 fix(docker-compose.yml): add NODE_ENV=production environment variable to app service 🔧 chore(package.json): remove start:cache and start:server scripts, add start:redis script 🔧 chore(cache/index.ts): add createClientInstance method to handle different Redis client creation based on NODE_ENV value

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -7,9 +7,17 @@ export class RedisClientManager {
 
     private constructor() {}
 
+    private static createClientInstance() {
+        if (process.env.NODE_ENV !== 'development') {
+            return createClient({ url: 'redis://redis:6379' })
+        } else {
+            return createClient()
+        }
+    }
+
     public static getInstance(): RedisClient {
         if (RedisClientManager.instance == null) {
-            this.instance = createClient({ url: 'redis://redis:6379' })
+            this.instance = this.createClientInstance()
         }
 
         return this.instance
