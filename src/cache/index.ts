1e12238de145a31d84098566ed91aa0e85e59ba9
Chance Linz <clinz.dev@gmail.com>
2023-03-15 00:30:31 -0700

    📝 docs(cache/index.ts): add JSDoc comments to RedisClientManager class and its methods 🚀 chore(cache/index.ts): add support for creating redis client based on stage and make RedisClientManager a singleton class

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -2,11 +2,20 @@ import { createClient } from 'redis'
 
 type RedisClient = ReturnType<typeof createClient>
 
+/**
+ * A singleton for managing the connection to the redis cache
+ */
 export class RedisClientManager {
     private static instance: RedisClient
 
+    /** Private constructor so this class can't get accidentally newed up */
     private constructor() {}
 
+    /**
+     * Creates an instance of the redis client based on stage
+     *
+     * @returns redis client
+     */
     private static createClientInstance() {
         if (process.env.NODE_ENV !== 'development') {
             return createClient({ url: 'redis://redis:6379' })
@@ -15,6 +24,11 @@ export class RedisClientManager {
         }
     }
 
+    /**
+     * Gets the instance if one exists, otherwise creates one
+     *
+     * @returns redis client
+     */
     public static getInstance(): RedisClient {
         if (RedisClientManager.instance == null) {
             this.instance = this.createClientInstance()
