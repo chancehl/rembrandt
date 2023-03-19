08a0b43cfd105d2e9ff3c25bb3d9d69caabbb41d
Chance Linz <clinz.dev@gmail.com>
2023-03-19 00:34:35 -0700

    🔨 refactor(bot.ts): remove registerCommands from bot ready event and move it to onClientReady handler ✨ feat(onClientReady.ts): add registerCommands function to register slash commands on bot ready event

--- src/handlers/onClientReady.ts
+++ src/handlers/onClientReady.ts
@@ -1,9 +1,14 @@
 import { Client } from 'discord.js'
 
+import { registerCommands } from './registerCommands'
+
 /**
  * This gets invoked when the bot successfully joins a guild and emits the onReady event
  */
 export async function onClientReady(bot: Client<true>) {
+    // register commands
+    await registerCommands(bot)
+
     // for now, just log. we'll collect metrics & init callbacks, slash commands here in the future
     console.log(`Ready! Logged in as ${bot.user.tag}`)
 }
