06aa53e8abe3677053a3fc426f1e57e33339dfd3
Chance Linz <clinz.dev@gmail.com>
2023-03-19 18:45:13 -0700

    🚀 feat(subscribe.command.ts): add validation for channel type and error messages

--- src/commands/subscribe.command.ts
+++ src/commands/subscribe.command.ts
@@ -1,13 +1,27 @@
-import { CommandInteraction, SlashCommandChannelOption, ApplicationCommandType } from 'discord.js'
+import { CommandInteraction, SlashCommandChannelOption, ApplicationCommandType, ChannelType } from 'discord.js'
 
 import { Command } from './base.command'
 
-// TODO: this is a stubbed out command for now
 async function execute(interaction: CommandInteraction) {
-    // parse query
     const channel = interaction.options.get('channel')
 
-    console.log({ channel })
+    if (channel == null) {
+        await interaction.followUp({ content: 'I need to know which channel you want me to send updates to.' })
+
+        return
+    }
+
+    // channel.type is 0-15, these DO have overlap.
+    // TODO: remove the cast to unknown once this can be parsed into a ChannelType
+    const channelType = channel.type as unknown as ChannelType
+
+    if (channelType != ChannelType.GuildText) {
+        await interaction.followUp({ content: 'Sorry, I only support sending daily updates to **text channels** at the moment.' })
+
+        return
+    }
+
+    // TODO: register subscribe callback
 
     // reply
     await interaction.followUp({ content: 'Testing' })
@@ -16,7 +30,7 @@ async function execute(interaction: CommandInteraction) {
 // prettier-ignore
 const channelOption = new SlashCommandChannelOption()
     .setName("channel")
-    .setDescription("the channel to send your daily updates to")
+    .setDescription("the text channel to send your daily updates to")
     .setRequired(true)
 
 export const SubscribeCommand: Command = {
