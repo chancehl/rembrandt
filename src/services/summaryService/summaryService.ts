96ffd5675b528c3c567327daa0ed970111efe2cc
Chance Linz <clinz.dev@gmail.com>
2023-03-27 20:41:09 -0700

    🔒 chore(summaryService.ts): add private apiConfiguration property and ConstructorArgs type 🔒 chore(summaryService.ts): add optional apiConfiguration argument to constructor 🔒 chore(summaryService.ts): set apiConfiguration to default OpenAIApiConfiguration if not provided in constructor 🔒 chore(summaryService.ts): use apiConfiguration from constructor if provided, otherwise use default OpenAIApiConfiguration 🚀 feat(summaryService.ts): generate summary for object using OpenAIApi and apiConfiguration

--- src/services/summaryService/summaryService.ts
+++ src/services/summaryService/summaryService.ts
@@ -3,18 +3,26 @@ import { Configuration as OpenAIApiConfiguration, OpenAIApi } from 'openai'
 import { logger } from '../../logger'
 import { CollectionObject } from '../../types'
 
+type ConstructorArgs = {
+    apiConfiguration?: OpenAIApiConfiguration
+}
+
 export class SummaryService {
+    private apiConfiguration: OpenAIApiConfiguration = new OpenAIApiConfiguration({
+        organization: process.env.OPEN_AI_ORGANIZATON_ID,
+        apiKey: process.env.OPEN_AI_API_KEY,
+    })
+
     private createObjectSummaryPrompt = `Write a summary (like you'd seed on a placard at a museum) for the Metropolitan Museum of Art collection piece: PIECE_IDENTIFIER.`
 
-    constructor() {}
+    constructor(args?: ConstructorArgs) {
+        if (args?.apiConfiguration) {
+            this.apiConfiguration = args.apiConfiguration
+        }
+    }
 
     async generateSummary(object: CollectionObject) {
-        const openai = new OpenAIApi(
-            new OpenAIApiConfiguration({
-                organization: process.env.OPEN_AI_ORGANIZATON_ID,
-                apiKey: process.env.OPEN_AI_API_KEY,
-            }),
-        )
+        const openai = new OpenAIApi(this.apiConfiguration)
 
         logger.info(`Generating summary for object ${object.objectID}`)
 
