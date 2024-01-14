3f32c964e6bda5a243b5473e07e4a62d5f7fa3aa
Chance Linz <clinz.dev@gmail.com>
2024-01-14 01:42:19 -0800

    updating summaryService.ts to use latest openai api

--- src/services/summaryService/summaryService.ts
+++ src/services/summaryService/summaryService.ts
@@ -1,50 +1,43 @@
-import { Configuration as OpenAIApiConfiguration, OpenAIApi } from 'openai'
+import OpenAI from 'openai'
 
 import { logger } from '../../logger'
 import { CollectionObject } from '../../types'
 
 type ConstructorArgs = {
-    apiConfiguration?: OpenAIApiConfiguration
+    openai?: OpenAI
 }
 
 export class SummaryService {
-    private apiConfiguration: OpenAIApiConfiguration = new OpenAIApiConfiguration({
+    private openai: OpenAI = new OpenAI({
         organization: process.env.OPEN_AI_ORGANIZATON_ID,
         apiKey: process.env.OPEN_AI_API_KEY,
     })
 
-    private createObjectSummaryPrompt = `Write a summary (like you'd seed on a placard at an art museum) for the Metropolitan Museum of Art collection piece: PIECE_IDENTIFIER.`
+    private prompt = `You are an art historian working at the Metropolitan Museum of Art. Write a brief summary (as if you were giving a tour of the museum) describing the following piece in your collection: PIECE_IDENTIFIER.`
 
     constructor(args?: ConstructorArgs) {
-        if (args?.apiConfiguration) {
-            this.apiConfiguration = args.apiConfiguration
+        if (args?.openai) {
+            this.openai = args.openai
         }
     }
 
     async generateSummary(object: CollectionObject) {
-        const openai = new OpenAIApi(this.apiConfiguration)
-
         logger.info(`Generating summary for object ${object.objectID}`)
 
-        const response = await openai.createChatCompletion({
+        const response = await this.openai.completions.create({
             model: 'gpt-3.5-turbo',
-            messages: [
-                {
-                    role: 'user',
-                    content: this.generatePrompt(object),
-                },
-            ],
+            prompt: this.generatePrompt(object),
         })
 
-        logger.info(`Generated summary for object ${object.objectID}, usage=${JSON.stringify(response.data.usage)}`)
+        logger.info(`Generated summary for object ${object.objectID}, usage=${JSON.stringify(response.usage)}`)
 
-        const contents = response.data.choices.map((choice) => choice.message?.content)
+        const contents = response.choices.map((choice) => choice.text)
 
         return contents.join('\n')
     }
 
     private generatePrompt(object: CollectionObject) {
-        const tokenizedTemplate = this.createObjectSummaryPrompt.replace('PIECE_IDENTIFIER', `${object.title}, ${object.medium}, ${object.department}`)
+        const tokenizedTemplate = this.prompt.replace('PIECE_IDENTIFIER', `${object.title}, ${object.medium}, ${object.department}`)
 
         return tokenizedTemplate
     }
